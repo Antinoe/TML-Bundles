c3e94c32865d42b211eefb0a2d6cad09818bc15f
Antinoe Hadrian <88167211+Antinoe@users.noreply.github.com>
2023-03-03 16:54:35 -0500

    Remove or Revise Redundant Comments

--- Items/BaseBundle.cs
+++ Items/BaseBundle.cs
@@ -7,19 +7,19 @@ using Terraria.Audio;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ModLoader.IO;
-using Microsoft.Xna.Framework; //Text Colors.
-using Microsoft.Xna.Framework.Input; //Allows Left Shift to be detected.
+using Microsoft.Xna.Framework; //	Text Colors.
+using Microsoft.Xna.Framework.Input; //	Allows Left Shift to be detected.
 
 
 namespace Bundles.Items
 {
 	public class BaseBundle : ModItem
 	{
-		// Properties changed by the other bundles
+		//	Properties changed by the other bundles
 		protected virtual int maxCapacity() => 999;
 		virtual protected bool ValidContainedItem(Item item)
         {
-			return item.maxStack > 1; // If the item isn't unstackable.
+			return item.maxStack > 1; //	If the item isn't unstackable.
         }
 
 		private List<Item> bundleList = new List<Item>();
@@ -44,28 +44,28 @@ namespace Bundles.Items
 		
 		public override void ModifyTooltips(List<TooltipLine> tooltips)
 		{
-            Player player = Main.player[Main.myPlayer]; //This is here so we can detect if the Player is using certain controls.
+            Player player = Main.player[Main.myPlayer]; //	This is here so we can detect if the Player is using certain controls.
 			
 			int currentCapacity = 0;
 			foreach (var item in bundleList)
 			{
 				currentCapacity += item.stack;
 			}
-			TooltipLine CurrentCapacity = new TooltipLine(Mod, "CurrentCapacity", $"Capacity: {currentCapacity}/{maxCapacity()}"); //Displays the Current Capacity of the Bundle.
+			TooltipLine CurrentCapacity = new TooltipLine(Mod, "CurrentCapacity", $"Capacity: {currentCapacity}/{maxCapacity()}"); //	Displays the Current Capacity of the Bundle.
 			tooltips.Add(CurrentCapacity);
-			CurrentCapacity.OverrideColor = Color.Cyan; //Color of the Capacity.
+			CurrentCapacity.OverrideColor = Color.Cyan; //	Color of the Capacity.
 			
 			TooltipLine ShowControls = new TooltipLine(Mod, "ShowControls", "Right-Click with items in hand to stow them." + "\nRight-Click with an empty hand to withdraw items." + "\nShift-Right-Click to withdraw in reverse order.");
-			ShowControls.OverrideColor = Color.Orange; //Color of the Controls Preview.
+			ShowControls.OverrideColor = Color.Orange; //	Color of the Controls Preview.
 			
-			if (player.controlUp) //If holding Up.
+			if (player.controlUp) //	If holding Up.
 			{
 				tooltips.Add(ShowControls);
 			}
 			
 			foreach (Item item in Enumerable.Reverse<Item>(this.bundleList))
 			{
-				tooltips.Add(new TooltipLine(Mod, "ItemInfo", item.HoverName)); //Displays each item in the Item List.
+				tooltips.Add(new TooltipLine(Mod, "ItemInfo", item.HoverName)); //	Displays each item in the Item List.
 			}
 		}
 		
@@ -82,25 +82,26 @@ namespace Bundles.Items
 			}
 		}
 		
-		public override bool ConsumeItem(Player player) //Prevents the item from vanishing upon stowing items.
+		//	Prevents the item from vanishing upon stowing items.
+		public override bool ConsumeItem(Player player)
 		{
 			return false;
 		}
 		
-		public override bool CanRightClick() //Allows the bundle's main features to function.
+		//	RightClick functions (inserting/extracting) cannot work without this.
+		public override bool CanRightClick()
 		{
 			return true;
 		}
 		
 		public override void RightClick(Player player)
 		{
-			int currentCapacity = 0; //Total Capacity.
+			int currentCapacity = 0;
 			foreach (var Item in bundleList)
 			{
 				currentCapacity += Item.stack;
 			}
 			
-			//This is MUCH more efficient than what was used in the last version.
 			if (player.whoAmI == Main.myPlayer)
 			{
 				//	Inserting
@@ -117,7 +118,7 @@ namespace Bundles.Items
 				//	Extracting
 				else
 				{
-					if (this.bundleList.Count != 0) //If the bundle contains an item or more.
+					if (this.bundleList.Count != 0)
 					{
 						BundleExtract();
 					}
@@ -139,13 +140,13 @@ namespace Bundles.Items
 			var player = Main.LocalPlayer;
 			
 			SoundEngine.PlaySound(Sounds.Item.BundleExtract, player.position);
-			if (Main.keyState.IsKeyDown(Keys.LeftShift)) //If holding Left Shift.
+			if (Main.keyState.IsKeyDown(Keys.LeftShift))
 			{
 				Item item = Enumerable.First<Item>(this.bundleList);
 				Main.mouseItem = item.Clone();
 				this.bundleList.Remove(item);
 			}
-			else //If not holding Left Shift.
+			else
 			{
 				Item item = Enumerable.Last<Item>(this.bundleList);
 				Main.mouseItem = item.Clone();
@@ -160,27 +161,26 @@ namespace Bundles.Items
 			
 			SoundEngine.PlaySound(Sounds.Item.BundleDump, player.position);
 			Item item = Enumerable.Last<Item>(this.bundleList);
-			// @TODO: Probably .Clone() is redundant should be cloned by the spawn function
+			//	@TODO: Probably .Clone() is redundant should be cloned by the spawn function
 			player.QuickSpawnClonedItem(source, item.Clone(), item.stack);
 			this.bundleList.Remove(item);
 		}
 		
-		//Start: Code that disallows the "Universal Item List" bug.
+		//	Start: Code that disallows the "Universal Item List" bug.
 		public override void OnCreate(ItemCreationContext context)
         {
 			bundleList = new List<Item>();
         }
 		
-		//Make sure to change the "BaseBundle" term within this to the name of your custom bundle.
         public override ModItem Clone(Item Item)
         {
             BaseBundle clone = (BaseBundle)base.Clone(Item);
             clone.bundleList = bundleList.Select(Item => Item.Clone()).ToList();
             return clone;
         }
-		//End: Code that disallows the "Universal Item List" bug.
+		//	End: Code that disallows the "Universal Item List" bug.
 		
-		//Start: Save/Load Data stuff.
+		//	Start: Save/Load Data stuff.
 		public override void SaveData(TagCompound tag)
 		{
 			List<TagCompound> bundleList = new List<TagCompound>();
@@ -218,6 +218,6 @@ namespace Bundles.Items
 				this.bundleList.Add(ItemIO.Receive(reader, true, false));
 			}
 		}
-		//End: Save/Load Data stuff.
+		//	End: Save/Load Data stuff.
 	}
 }
